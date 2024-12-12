3bafa4d285879d9289804112ed779e084f2980dc
Tuhin Ahmed <mdtuhin.dev@gmail.com>
2024-12-12 18:29:46 +0600

    fix: modify dbConnect

--- service/dbConnect.js
+++ service/dbConnect.js
@@ -1,12 +1,30 @@
 import mongoose from "mongoose";
 
-export async function dbConnect() {
-   try {
-      const conn = await mongoose.connect(
-         String(process.env.MONGODB_CONNECTION_STRING)
-      );
-      return conn;
-   } catch (err) {
-      console.error(err);
-   }
-}
+const MONGODB_URI = process?.env?.MONGODB_CONNECTION_STRING;
+const cached = {};
+
+export const dbConnect = async () => {
+  if (!MONGODB_URI) {
+    throw new Error(
+      "Please define the MONGODB_URI environment variable inside .env.local"
+    );
+  }
+  if (cached.connection) {
+    return cached.connection;
+  }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+    cached.promise = mongoose.connect(MONGODB_URI, opts);
+  }
+  try {
+    cached.connection = await cached.promise;
+  } catch (e) {
+    cached.promise = undefined;
+    console.error(e);
+    throw e;
+  }
+
+  return cached.connection;
+};
